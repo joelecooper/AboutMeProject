afdc111b5a89650372199f2ecc6e53aae6b90f9f
Joel Cooper <joel.e.cooper>
2021-03-09 21:26:12 -0600

    added code to the html, java, and css of my project page.

---
+++ js/app.js
@@ -0,0 +1,59 @@
+'use strict'
+
+function getBladeRunner(){
+    let bladeRunner = prompt('Are you a fan of the Blade Runner movies? Yes/No');
+    
+    if (bladeRunner == 'Yes'){
+        alert('Welcome, Blade Runner.');
+    }   else {
+        alert('You must be a Replicant.');
+    }
+        console.log(bladeRunner.toLocaleLowerCase());
+}
+
+function getBaseline(){
+    let baseline = prompt('Do you remember your baseline? Yes/No');
+
+    if (baseline == "Yes"){
+        alert('Well done, you can pick up your bonus.');
+    }   else {
+        alert('Remember: Cells, interlinked.')
+    }
+}
+
+function getCBeams(){
+    let cBeams = prompt('Have you ever watched C-beams glitter in the dark near the Tannhauser gate? Yes/No')
+
+    if (CBeams == "Yes"){
+        alert('All those moments will be lost in time, like tears in rain.');
+    }
+        else{
+        alert('I\'ve seen things you people wouldn\'t believe.')
+    }
+}
+
+function get(){
+    let bladeRunner = prompt('Are you a fan of the Blade Runner movies? Yes/No');
+    
+    if (bladeRunner == 'Yes'){
+        alert('Welcome, Blade Runner.');
+    }   else {
+        alert('You must be a Replicant.');
+    }
+}
+
+function get(){
+    let bladeRunner = prompt('Are you a fan of the Blade Runner movies? Yes/No');
+    
+    if (bladeRunner == 'Yes'){
+        alert('Welcome, Blade Runner.');
+    }   else {
+        alert('You must be a Replicant.');
+    }
+}
+
+getBladeRunner()
+
+getBaseline()
+
+getCBeams()
